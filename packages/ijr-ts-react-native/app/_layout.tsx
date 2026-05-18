9563291173d615cb0090eaa1f6b39bbd0dd2a37c
danielmorbeck <danielmorbeck@gmail.com>
2026-05-18 08:02:54 -0300

    chore: added Reactotron for debugging

--- packages/ijr-ts-react-native/app/_layout.tsx
+++ packages/ijr-ts-react-native/app/_layout.tsx
@@ -1,3 +1,5 @@
+import './_dev';
+
 import { ApolloProvider } from '@apollo/client/react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
